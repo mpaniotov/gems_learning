609744ab8af5cc63b699cb3b3d4c03e1d069866c
mpaniotov <elteck81@gmail.com>
2014-05-26 15:33:22 +0300

    some changes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,5 @@
-deploy_to  = "/srv/unicornnginx"
+deploy_to  = "/home/avior/srv/unicornnginx"
+
 rails_root = "#{deploy_to}/current"
 pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/unicorn.sock"
